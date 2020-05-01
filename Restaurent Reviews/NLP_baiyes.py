4b65a6a8c47825fe1358f2fe562a38f12ab7a85b
lakshya5079 <56447554+lakshya5079@users.noreply.github.com>
2020-05-01 21:06:42 +0530

    added Restaurent Reviews code

---
+++ Restaurent Reviews/NLP_baiyes.py
@@ -0,0 +1,50 @@
+# Natural Language Processing
+#Importing the libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Importing the Dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
+
+#cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+ps= PorterStemmer() 
+corpus= []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-z]',' ',dataset['Review'][i])
+    review = review.lower()
+    review=review.split()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+#making the sparse matrix
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer()
+X= cv.fit_transform(corpus).toarray()
+y=dataset.iloc[:,-1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+acc= 1- (20+50)/250
+print(acc)
+
